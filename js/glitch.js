6ff6b0beaa16ddc8c5c8723c818243b659356500
A-S-T-E-R <marcsantossayo@gmail.com>
2025-11-25 08:29:46 +0800

    Update glitch.js

--- js/glitch.js
+++ js/glitch.js
@@ -1,6 +1,3 @@
-// Glitch effect initialization
 document.addEventListener('DOMContentLoaded', function() {
-    // This file is for any additional glitch effect customizations
-    // The main glitch effects are handled via CSS
     console.log('Glitch effects initialized');
 });
